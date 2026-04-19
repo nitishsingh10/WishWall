201e8a2b3ce07c7a3bae31f1e1e2fc323b043e26
nitishsingh10 <nitishsingh9hfhs@gmail.com>
2026-04-19 19:46:08 +0530

    added js logic

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,28 +1,43 @@
-const btn = document.getElementById("post-btn");
-const feed = document.getElementById("feed-container");
+const btn = document.getElementById("message-btn");
+const feed = document.getElementById("message-displaying");
+const posts = [];
+
+// User post logic 
 
 btn.addEventListener('click',()=>{
 
-    const userName = document.getElementById("name-input")
+    // Inputs of user 
+
+    const userName = document.getElementById("Username")
                     .value
                     .trim();
 
-    const message = document.getElementById("message-input")
+    const message = document.getElementById("msg")
                     .value
                     .trim();
+
+
+    // Storing user data in object array for future use.
 
     const postObj = {name:`${userName}`, text : `${message}`};
     
+    posts.push(postObj);
 
     const post = document.createElement("div");
 
-    post.classList.add("post");
+    //class list for styling
+
+    post.classList.add("user-post");
+
+    // appending the message
 
     post.innerHTML = `<h4>${postObj.name}</h4>
                     <p>${postObj.text}</p>`;
 
     feed.prepend(post);
 
+
+    // clear exisiting inputs
     clear();
 
 })
@@ -30,9 +45,9 @@ btn.addEventListener('click',()=>{
 
 function clear(){
 
-    document.getElementById("name-input")
+    document.getElementById("Username")
     .value = "";
-    document.getElementById("message-input")
+    document.getElementById("msg")
     .value = "";
 
 }
