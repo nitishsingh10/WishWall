e44a9e320510a9b0f0efb8a81caaca71dd4b9969
nitishsingh10 <nitishsingh9hfhs@gmail.com>
2026-05-05 00:26:37 +0530

    day-2 backend test success

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,23 +1,37 @@
 const express = require('express');
-const { message } = require('statuses');
 const router = express.Router();
+const User = require('../models/user.model');
 
 router.post('/signup', async (req, res) => {
-  const { username, email, password } = req.body;
-
-        if(!username || !email || !password){
-            return res.json({
-                    success : false,
-                    message : "All fields are required"
+        
+        try{
+                const { name, email, password } = req.body;
+                if(!name || !email || !password){
+                    return res.json({
+                            success : false,
+                            message : "All fields are required"
+                        });
+                }
+        
+                const user = new User({
+                    name,
+                    email,
+                    password
                 });
+        
+                await user.save()
+        
+                res.json({success : true, message : "signup working"});
         }
-        res.json({success : true, message : "signup working"});
+        catch(error){
+                res.json({ success: false, message: "Error saving user" });
+        }
 
 });
 
 router.post('/login',async (req, res)=>{
 
-        const {username, password} = req.body;
+        const {name, password} = req.body;
 
         if(!username || !password){
                 return res.json({
